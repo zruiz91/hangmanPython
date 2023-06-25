03dbe95fdb67d64a439ddd1f06680beb273c8ed6
Zach Ruiz <powersbooth10@gmail.com>
2023-06-25 16:18:42 -0500

    added some starter code.

---
+++ hangman.py
@@ -0,0 +1,13 @@
+#Step 1
+import random
+
+
+word_list = ["aardvark", "baboon", "camel"]
+
+#Todo-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+#Todo-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+#Todo-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
