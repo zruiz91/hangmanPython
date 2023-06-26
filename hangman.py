0e7ea8bdaae6f39492565f454db7b57515bc345d
Zach Ruiz <powersbooth10@gmail.com>
2023-06-26 13:18:59 -0500

    refactored code to print the display list variable as a string.

--- hangman.py
+++ hangman.py
@@ -76,7 +76,7 @@ for letter in chosen_word:
 
 
 print(stages[lives])
-print(display)
+print(f"{' '.join(display)}")
 
 #While loop to keep the user guessing until completed
 while not end_of_game:
@@ -93,19 +93,20 @@ while not end_of_game:
     #Checks if guess was wrong and detreacts from their life score if so.
     if guess not in chosen_word:
         lives -= 1
+        # Checks if user still has life if not game over
+        if lives == 0:
+            end_of_game = True
+            print("You lose, loser.")
 
 
 
-    print(stages[lives])
-    print(display)
+    # prints the display list variable as a string
+    print(f"{' '.join(display)}")
 
-
-    # Checks if user still has life if not game over
-    if lives == 0:
-        end_of_game = True
-        print("You lose, loser.")
 
     # checks if user has guessed all the blanks and if so wins
     if "_" not in display:
         end_of_game = True
         print("You've won.")
+
+    print(stages[lives])
